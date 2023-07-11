99ef7a5e064b72d875720d18e1d3c125bd48deb6
unknown <jyygxxf@outlook.com>
2023-07-11 22:47:37 +0800

    Modbus_CRC16

--- Modbus_CRC16.c
+++ Modbus_CRC16.c
@@ -4,7 +4,7 @@ typedef __uint8_t u8;
 typedef __uint16_t u16;
 typedef __uint32_t u32;
 
-u16 CRC16(u8 *data, size_t length, u16 CRC_register, u16 polynomial)
+u16 Modbus_CRC16(u8 *data, size_t length, u16 CRC_register, u16 polynomial)
 {
     for (size_t i = 0; i < length; i++)
     {
@@ -26,6 +26,6 @@ u16 CRC16(u8 *data, size_t length, u16 CRC_register, u16 polynomial)
 int main(void)
 {
     u8 data[5] = {1, 4, 2, 0xff,0xff};
-    printf("%x", CRC16(data, sizeof(data), 0xFFFF, 0xA001));
+    printf("%x", Modbus_CRC16(data, sizeof(data), 0xFFFF, 0xA001));
     return 0;
 }
